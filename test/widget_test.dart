aa0a56cae0fa62b3355bb61c10a66bf15ab88a00
mr-shakib <contactshakibhere@gmail.com>
2026-02-28 21:10:14 +0600

    fix: update widget test to use DarazApp instead of MyApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:daraz_ui/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( DarazApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
